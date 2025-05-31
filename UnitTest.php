7844cfc70cb0b3466efd55ba0547e263ca66ec9b
BETEPAH42 <62057900+BETEPAH42@users.noreply.github.com>
2025-05-31 08:44:25 +0300

    Update UnitTest.php
    Доработка

--- UnitTest.php
+++ UnitTest.php
@@ -1,32 +1,31 @@
 <?php
-		require_once '../reversText.php';
+require_once '../reversText.php';
 		
-		class Test extends Revert
-		{
-				public function test()
-				{
-					$test_str = "hello";
-					$this->str = $test_str;
+class Test extends Revert
+{
+	static function test(string $str) : bool
+	{
+		$this->str = $str;
 					
-					$arr_rev_step = str_split($test_str, 1);
-					$arr_rev = array_reverse($arr_rev_step);
+		$arr_rev_step = str_split($test_str, 1);
+		$arr_rev = array_reverse($arr_rev_step);
 
+		$flag = true;
+
+		for ($i=0; $i < count($arr_rev_step); $i++) { 
+			for ($j=count($arr_rev_step)-1; $j >= 0; $j--) { 
+				if ($arr_rev_step[$i] == $arr_rev[$j]) {
 					$flag = true;
-
-					for ($i=0; $i < count($arr_rev_step); $i++) 
-					{ 
-						for ($j=count($arr_rev_step)-1; $j >= 0; $j--) 
-						{ 
-							if ($arr_rev_step[$i] == $arr_rev[$j])
-							{
-								$flag = true;
-							} else
-							{
-								$flag = false;
-								return $flag;
-							}
-						}
-					}
+				}
+				else {
+					$flag = false;
 					return $flag;
-				} 
+				}
+			}
 		}
+	return $flag;
+	} 
+}
+
+$testUnit = Test::test("hello");
+var_dump($testUnit);
